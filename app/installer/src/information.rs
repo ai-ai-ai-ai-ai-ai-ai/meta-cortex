3c60819d6062d543fee90f7337f4e8fbc41f79f5
cypherkitty <bynull@meta-secret.org>
2026-09-24 01:28:14 -0700

    release: prepare v0.8.0

--- app/installer/src/information.rs
+++ app/installer/src/information.rs
@@ -26,6 +26,7 @@ pub enum VersionError {
 pub enum Version {
     V0_6_2,
     V0_7_0,
+    V0_8_0,
 }
 
 #[derive(Debug, PartialEq, Eq, Error)]
@@ -58,6 +59,7 @@ impl Version {
         match text.as_bytes() {
             b"0.6.2" => Ok(Version::V0_6_2),
             b"0.7.0" => Ok(Version::V0_7_0),
+            b"0.8.0" => Ok(Version::V0_8_0),
             _ => Err(VersionTextError::Unsupported),
         }
     }
@@ -82,6 +84,7 @@ impl Version {
         match self {
             Self::V0_6_2 => "0.6.2",
             Self::V0_7_0 => "0.7.0",
+            Self::V0_8_0 => "0.8.0",
         }
     }
 
@@ -206,8 +209,8 @@ pub mod tests {
                 Err(VersionTextError::Unsupported)
             );
         }
-        assert_eq!(Version::CURRENT, Version::V0_7_0);
-        for version in [Version::V0_6_2, Version::V0_7_0] {
+        assert_eq!(Version::CURRENT, Version::V0_8_0);
+        for version in [Version::V0_6_2, Version::V0_7_0, Version::V0_8_0] {
             let text = version.as_str().to_owned();
             assert_eq!(Version::try_from(text.clone()), Ok(version));
             assert_eq!(Version::try_from(format!("{text}\n")), Ok(version));
